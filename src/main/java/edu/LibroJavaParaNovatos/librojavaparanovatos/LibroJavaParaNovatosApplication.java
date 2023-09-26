a1e6a718d8cf2591af83b69a4e23b0738607f17a
Saul Echeverri <secheverri@cidenet.com.co>
2023-09-26 06:11:29 -0500

    :tada: Primer commit

---
+++ src/main/java/edu/LibroJavaParaNovatos/librojavaparanovatos/LibroJavaParaNovatosApplication.java
@@ -0,0 +1,13 @@
+package edu.LibroJavaParaNovatos.librojavaparanovatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroJavaParaNovatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroJavaParaNovatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/LibroJavaParaNovatos/librojavaparanovatos/LibroJavaParaNovatosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.LibroJavaParaNovatos.librojavaparanovatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibroJavaParaNovatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
