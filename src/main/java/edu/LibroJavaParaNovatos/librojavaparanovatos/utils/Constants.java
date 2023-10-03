40b243594e81d0b84599cbcea148832f32aa03ce
Saul Echeverri <secheverri@cidenet.com.co>
2023-10-03 10:12:36 -0500

    :memo: Finalizado el ejercicio 2.4

---
+++ src/main/java/edu/LibroJavaParaNovatos/librojavaparanovatos/utils/Constants.java
@@ -0,0 +1,9 @@
+package edu.LibroJavaParaNovatos.librojavaparanovatos.utils;
+
+public class Constants {
+
+    public static final double MILLAS_A_KILOMETROS = 1.60934;
+    public static final double MILLAS_A_METROS = 1609.34;;
+
+
+}
